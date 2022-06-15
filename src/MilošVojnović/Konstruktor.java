5bb06acb0229f46dbf3156e479e9b3f43380c116
mvojnovic <Lipovgaj179>
2022-06-15 15:50:33 +0200

    Project tasks-Miloš Vojnović
    In this project I will try to teach Java in the best possible way.
    In these two weeks, I will go through the basic elements of language.
    And in the end, I will complete my knowledge with the final task.

--- src/MilošVojnović/Konstruktor.java
+++ src/MilošVojnović/Konstruktor.java
@@ -1,4 +1,20 @@
 package MilošVojnović;
 
-public class Konstruktor {
+public class Konstruktor {int x;
+/*
+    //This is Constructor - same name as Class name
+    public Konstruktor() {
+        x = 10;
+    }
+
+    public static void main(String[] args) {
+
+       /* Main newObj = new Main();
+
+        System.out.println(newObj.x);
+
+        newObj.x = 555;
+
+        System.out.println(newObj.x);
+    }*/
 }
